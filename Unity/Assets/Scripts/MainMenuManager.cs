3f7aa824424ac2056df9c69b96317417e349ac82
Trevor <reevest2@gmail.com>
2024-10-05 22:00:06 -0400

    main menu manager

---
+++ Unity/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void OnClickStart()
+    {
+        SceneManager.LoadScene("Minigame");
+    }
+
+    public void OnClickSettings()
+    {
+        // TODO: Open Settings window
+    }
+
+    public void OnClickCredits()
+    {
+        // TODO: Open Credits window
+    }
+}
