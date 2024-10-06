09c06046c22bbd4fa35eeefba738a5afd74012cf
Trevor <reevest2@gmail.com>
2024-10-06 00:48:43 -0400

    settings wip

--- Unity/Assets/Scripts/EventManager.cs
+++ Unity/Assets/Scripts/EventManager.cs
@@ -4,8 +4,6 @@ public class EventManager
 {
     public static event UnityAction OnAlgaeCollected;
     public static event UnityAction OnCoinCollected;
-    public static event UnityAction OnLevelCompleted;
     public static void AlgaeCollected() => OnAlgaeCollected?.Invoke();
     public static void CoinCollected() => OnCoinCollected?.Invoke();
-    public static void LevelCompleted() => OnLevelCompleted?.Invoke();
 }
