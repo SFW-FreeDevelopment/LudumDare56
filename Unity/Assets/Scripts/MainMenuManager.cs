505f3fe2896eddeb32899e138fbcb8e217ab6fe4
Steven Garberg <steven.garberg.jr@gmail.com>
2024-10-06 21:13:43 -0400

    Revert "Completed levels"
    This reverts commit 7edaea7ab86e560749580cbf589e11070e27d900.

--- Unity/Assets/Scripts/MainMenuManager.cs
+++ Unity/Assets/Scripts/MainMenuManager.cs
@@ -18,7 +18,7 @@ public class MainMenuManager : MonoBehaviour
         ShowMainMenu();
 
         // Hard-code the visibility of the 6th level button to false for now
-        level6Button.SetActive(GameManager.Instance.GetCoinCount() >= 5);
+        level6Button.SetActive(false); // TODO: Hook up to player progress and save data
     }
 
     // Show the main menu panel and hide others
